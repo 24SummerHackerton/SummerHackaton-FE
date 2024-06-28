28795cec27292b86d88b4e3e48244408d51c8209
ggsong0328 <ggsong0328@gmail.com>
2024-06-28 16:55:51 +0900

    exexexex

--- src/desktop/desktopViews/DesktopCreate.jsx
+++ src/desktop/desktopViews/DesktopCreate.jsx
@@ -1,6 +1,9 @@
 export default function DesktopCreate() {
   return (
     <div>
+      <div>
+        <h1>종목 생성 페이지</h1>
+      </div>
       <form>
         <div>
           <span>종목명</span>
@@ -11,9 +14,10 @@ export default function DesktopCreate() {
           <input type="number" placeholder="참가 인원을 입력하세요" />
         </div>
         <div>
-          <span>규칙</span>
-          <input type="text" />
+          <div>규칙</div>
+          <textarea placeholder="규칙을 적어주세요" />
         </div>
+        <button>완료</button>
       </form>
     </div>
   );
